db6083e33bf186e0af8083d35efb19f38152963c
sieun0 <85726398+sieun0@users.noreply.github.com>
2023-06-29 20:12:40 +0900

    makepage select 추가

--- src/views/QuizMakePage.vue
+++ src/views/QuizMakePage.vue
@@ -8,6 +8,7 @@
             @drop="handleDrop"
         >
             <p>업로드할 파일을 드래그 해주세요.</p>
+            <input type="file" ref="fileInput" multiple style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; opacity: 0; cursor: pointer">
         </div>
         <ul>
             <li v-for="file in files" :key="file.name">
@@ -40,15 +41,21 @@ export default{
         event.target.classList.remove("dragover");
 
         const fileList = event.dataTransfer.files;
-
-        for (let i = 0; i < fileList.length; i++) {
-            const file = fileList[i];
-            if (file.type === "application/pdf") {
-            this.files.push(file);
-            }
-        }
-
+        this.handleFiles(fileList);
         },
+        handleFileSelect(event) {
+            const fileList = event.target.files;
+            this.handleFiles(fileList);
+            this.$refs.fileInput.value = null; 
+        },
+        handleFiles(fileList) {
+            for (let i = 0; i < fileList.length; i++) {
+                const file = fileList[i];
+                if (file.type === "application/pdf") {
+                    this.files.push(file);
+                }
+            }   
+        }
     },
 };
 </script>
