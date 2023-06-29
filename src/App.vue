db97cebb1f81bf7f6117c68466e2905efa7d9cdb
sieun0 <85726398+sieun0@users.noreply.github.com>
2023-06-29 14:32:26 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id = "app">
+        <Header />
+
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/common/commonheader.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    name:'App',
+    components: {
+        Header,
+        
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+html.body{padding:0; margin:0;}
 </style>
---
+++ src/components/common/commonheader.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1><a href="#" class ="logo"><h1>Youngja</h1>
+            <div class ="menu">
+              <ul class="menulist">
+                <li><a href = "javascript:;" @click="createExam" : class="{'active' : isActive('create')}">시험지 만들기</a></li>
+                <li><a href = "javascript:;" @click="viewExam" : class="{'active' : isActive('view')}">내 시험지함</a></li>
+              </ul>
+            </div>
+    </header>
+</template>
+
+<script>
+export default {
+  method: {
+    createExam() {
+      //클릭 시 실행되는 곳
+    },
+    viewExam() {
+      //클릭 시 실행되는 곳
+    }
+  }
+}
+</script>
+
+<style scoped>
+header{width:100%; text-align:center; position:relative; height:120px; border-bottom:1px solid #35495e}
+header h1{position:absolute; top:0; left:50px;}
+header ul.menulist:after{display:block; clear:both; content:'';}
+header ul.menulist{position: absolute; top:20px; right:50px;}
+header ul.li{float:left; padding:10px 20px; list-style: none;}
+a{text-decoration:none; color:#333};
+</style>
