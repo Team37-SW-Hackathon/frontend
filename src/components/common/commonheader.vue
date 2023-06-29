a0bb6bf9e4216dc5e84835fb7e5b27c0d4a481ae
sieun0 <85726398+sieun0@users.noreply.github.com>
2023-06-29 17:21:19 +0900

    view

--- src/components/common/commonheader.vue
+++ src/components/common/commonheader.vue
@@ -12,7 +12,7 @@
 
 <script>
 export default {
-  data() {
+  data() {  
     return {
       activeItem: '',
     };
@@ -32,55 +32,4 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-header {
-  width: 100%;
-  text-align: center;
-  position: relative;
-  height: 60px;
-  border-bottom: 3px solid #35495e;
-}
-
-header h1 {
-  position: absolute;
-  top: 5px;
-  left: 15px;
-  font-size: 60px;
-}
-
-header ul.menulist {
-  position: relative;
-  top: 15px;
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-header ul.menulist li {
-  display: inline-block;
-  margin-right: 30px;
-}
-
-header ul.menulist li:last-child {
-  margin-right: 0;
-}
-
-header ul.menulist li a {
-  text-decoration: none;
-  color: #333;
-  padding: 10px 15px;
-  border-radius: 4px;
-  transition: background-color 0.3s ease;
-}
-
-header ul.menulist li a.active {
-  background-color: #35495e;
-  color: #fff;
-}
-
-header ul.menulist li a:hover {
-  background-color: #eaeaea;
-}
-</style>
 
